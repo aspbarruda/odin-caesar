c8e72187efdd11324265ccb60f6b61ca360db578
Andre <aspbarruda@gmail.com>
2022-11-11 06:05:12 -0300

    Finished exercise

---
+++ caesar.rb
@@ -0,0 +1,25 @@
+def caesar (string, shift_factor)
+	new_string = string.split("").map do |letter|
+		new_letter = letter
+		if letter.match(/[a-z]/i)
+			new_letter = string[letter].ord + shift_factor
+			if string[letter] == string[letter].upcase && new_letter > 90
+				new_letter = 64 + (new_letter - 90) #recyling through "A" if letter passed through "Z"
+			elsif string[letter] == string[letter].downcase && new_letter > 122
+				new_letter = 96 + (new_letter) - 122
+			end
+		end
+		new_letter.chr
+	end
+	new_string.join("")
+end
+
+puts "Enter with string to convert: "
+string = gets.chomp
+
+puts "Enter with shifting factor"
+shift = gets.chomp
+
+result = caesar(string, shift.to_i)
+
+puts result
